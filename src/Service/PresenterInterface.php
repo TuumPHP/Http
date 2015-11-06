f88991a929a953544276135e6d1387ead6eb53bb
Asao <kamei@workspot.jp>
2015-11-06 17:16:16 +0900

    using Presenter instead of ViewStream.

---
+++ src/Service/PresenterInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tuum\Respond\Service;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface PresenterInterface
+{
+    /**
+     * renders $view_file with $data.
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     * @param null|ViewData          $view
+     * @return ResponseInterface
+     */
+    public function render($request, $response, $view = null);
+}
