09fd0d4fa5fc4d12f7dabf57ec788bd2d9cc7e98
Asao <kamei@workspot.jp>
2016-02-26 13:23:25 +0900

    add a link to jQuery.

--- app/views/layouts/layout.php
+++ app/views/layouts/layout.php
@@ -18,6 +18,7 @@ $request = $this->view_data['server.request'];
 
     <!-- Latest compiled and minified JavaScript -->
     <!--suppress JSUnresolvedLibraryURL -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </head>
 <body>
