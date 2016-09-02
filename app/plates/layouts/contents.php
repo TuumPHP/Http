2116dcf6fb08249797b16b61dfe61ec3b9155221
Asao <kamei@workspot.jp>
2016-09-02 12:56:23 +0900

    simply pass all data to main layout from content-docs layout.

--- app/plates/layouts/contents.php
+++ app/plates/layouts/contents.php
@@ -5,11 +5,7 @@ use Tuum\Respond\Service\ViewHelper;
 /** @var Template $this */
 /** @var $view ViewHelper */
 
-$_view_data = [];
-if (isset($nav)) {
-    $_view_data['nav'] = $nav;
-}
-$this->layout('layouts/layout', $_view_data);
+$this->layout('layouts/layout', $this->data);
 
 ?>
 
