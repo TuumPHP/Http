add1bede7211ebd0915b18841bfe4a654f6bcb4e
Asao <kamei@workspot.jp>
2016-09-02 17:58:20 +0900

    add csrf token to login/logoff form inside the top nav-bar.

--- app/plates/layouts/UserHeaderLogIn.php
+++ app/plates/layouts/UserHeaderLogIn.php
@@ -7,6 +7,7 @@ use Tuum\Respond\Service\ViewHelper;
 ?>
 <!-- login form -->
 <form class="navbar-form navbar-left" role="search" action="/login" method="post">
+    <input type="hidden" name="_token" value="<?= $view->attributes('_token');?>" >
     <span class="text-primary">LogIn: <strong><?= isset($login) ? $this->escape($login): '???' ?></strong></span>
     <div class="form-group">
         <input type="hidden" name="logout" value="">
